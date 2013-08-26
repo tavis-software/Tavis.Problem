37d9d7d41cb61ac4f8e159b728468d61d4c10ef7
Darrel <darrel@tavis.ca>
2013-08-26 10:05:39 -0400

    Initial version of DOM

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Tavis.Problem
+
+This library provides .Net support for the media type `application/api-problem+json` http://tools.ietf.org/html/draft-nottingham-http-problem-04
+
+
---
+++ src/Problem/Problem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace Tavis
+{
+    public class Problem
+    {
+        public Uri ProblemType { get; set; }
+        public string Title { get; set; }
+
+        public HttpStatusCode StatusCode { get; set; }
+        public string Detail { get; set; }
+        public Uri ProblemInstance { get; set; }
+
+        public Dictionary<string, JToken> Extensions { get; set; } 
+    }
+}

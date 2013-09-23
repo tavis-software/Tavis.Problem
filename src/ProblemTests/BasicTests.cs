79e850284441b287bb4723e921ddb0a5d10431a9
Darrel <darrel@tavis.ca>
2013-09-23 15:58:30 -0400

    Added nuget packaging stuff

--- src/ProblemTests/BasicTests.cs
+++ src/ProblemTests/BasicTests.cs
@@ -56,8 +56,32 @@ namespace ProblemTests
             
         }
 
-        
 
+        [Fact]
+        public void ReturnAProblem()
+        {
+
+            var problem = new ProblemDocument
+            {
+                ProblemType = new Uri("http://example.org"),
+                Title = "Houston we have a problem",
+                StatusCode = HttpStatusCode.BadGateway,
+                ProblemInstance = new Uri("http://foo")
+            };
+
+            problem.Extensions.Add("bar", new JValue("100"));
+
+            var response = new HttpResponseMessage(HttpStatusCode.BadGateway)
+            {
+                Content = new ProblemContent(problem)
+            };
+
+
+            var problemString = response.Content.ReadAsStringAsync().Result;
+
+            Assert.NotEmpty(problemString);
+
+        }
 
     }
 }
